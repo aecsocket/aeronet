ad3c3e3db7e0348520c99269b147a91c9986d835
aecsocket <aecsocket@tutanota.com>
2023-09-02 13:01:10 +0100

    Get a basic wasm example

---
+++ aeronet_h3/examples/client.rs
@@ -0,0 +1,14 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                fit_canvas_to_parent: true,
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .run()
+}
