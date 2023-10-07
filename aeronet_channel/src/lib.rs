dd014c14ee46028af941ed39de59e7a34b529fb6
aecsocket <aecsocket@tutanota.com>
2023-10-07 22:24:00 +0100

    0.1.3

--- aeronet_channel/src/lib.rs
+++ aeronet_channel/src/lib.rs
@@ -1,10 +1,6 @@
 #![warn(clippy::all)]
 #![warn(missing_docs)]
 #![doc = include_str!("../README.md")]
-//! # Getting started
-//!
-//! The entry point to the crate is the server transport, as you cannot have a client without
-//! first creating a server. See [`ChannelTransportServer`] to get started.
 
 mod client;
 mod server;
