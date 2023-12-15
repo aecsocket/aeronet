12448744de73a8bfa03a1641ed167b5db855e567
aecsocket <aecsocket@tutanota.com>
2023-12-15 12:26:16 +0000

    WT WASM build files

---
+++ aeronet_wt_wasm/examples/echo_client.rs
@@ -0,0 +1,27 @@
+//!
+
+use bevy::prelude::*;
+use bevy_egui::{egui, EguiPlugin, EguiContexts};
+
+fn main() {
+    App::new()
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    fit_canvas_to_parent: true,
+                    prevent_default_event_handling: false,
+                    ..default()
+                }),
+                ..default()
+            }),
+            EguiPlugin,
+        ))
+        .add_systems(Update, ui)
+        .run();
+}
+
+fn ui(mut egui: EguiContexts) {
+    egui::CentralPanel::default().show(egui.ctx_mut(), |ui| {
+        ui.label("Hello world");
+    });
+}
--- aeronet_wt_wasm/src/lib.rs
+++ aeronet_wt_wasm/src/lib.rs
@@ -1,10 +1,6 @@
-#![warn(clippy::all)]
-#![warn(missing_docs)]
 #![doc = include_str!("../README.md")]
 
 mod bindings;
-mod client;
+//mod client;
 
-pub use aeronet_wt_core::*;
-
-pub use client::WebTransportClient;
+//pub use client::*;
