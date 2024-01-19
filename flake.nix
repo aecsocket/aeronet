26d9b6f325347128d25196f494470c97514fd843
aecsocket <aecsocket@tutanota.com>
2024-01-19 20:40:08 +0000

    Flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell rec {
+            nativeBuildInputs = [
+              pkg-config
+            ];
+            buildInputs = [
+              rustToolchain
+              llvmPackages.clangUseLLVM
+              mold
+              udev
+              alsa-lib
+              vulkan-loader
+              xorg.libX11
+              xorg.libXcursor
+              xorg.libXi
+              xorg.libXrandr
+              libxkbcommon
+              wayland
+            ];
+            LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          };
+        }
+      );
+}
