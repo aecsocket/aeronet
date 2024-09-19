6a1c4cc29f76ed7a63f8b975b3bec1dfd0f03e3f
aecsocket <aecsocket@tutanota.com>
2024-09-19 18:47:30 +0100

    webtransport client

--- crates/aeronet_channel/examples/simple.rs
+++ crates/aeronet_channel/examples/simple.rs
@@ -20,30 +20,30 @@ fn main() -> AppExit {
             ChannelIoPlugin,
         ))
         .add_systems(Startup, setup)
-        .add_systems(Update, ui)
+        .add_systems(Update, session_ui)
         .run()
 }
 
 #[derive(Debug, Default, Component)]
-struct UiState {
+struct SessionUi {
     msg: String,
     log: Vec<String>,
 }
 
 fn setup(mut commands: Commands) {
-    let a = commands.spawn((Name::new("A"), UiState::default())).id();
-    let b = commands.spawn((Name::new("B"), UiState::default())).id();
+    let a = commands.spawn((Name::new("A"), SessionUi::default())).id();
+    let b = commands.spawn((Name::new("B"), SessionUi::default())).id();
     commands.add(ChannelIo::open(a, b));
 }
 
-fn ui(
+fn session_ui(
     mut egui: EguiContexts,
     mut commands: Commands,
-    mut sessions: Query<(Entity, &Name, &mut UiState, &mut PacketBuffers)>,
+    mut sessions: Query<(Entity, &Name, &mut SessionUi, &mut PacketBuffers)>,
 ) {
     for (session, name, mut ui_state, mut bufs) in &mut sessions {
         for msg in bufs.drain_recv() {
-            let msg = String::from_utf8(Vec::from(msg)).unwrap();
+            let msg = String::from_utf8(msg.into()).unwrap_or_else(|_| "(not UTF-8)".into());
             ui_state.log.push(format!("> {msg}"));
         }
 
@@ -62,6 +62,13 @@ fn ui(
                 })
                 .inner;
 
+            if send_msg {
+                let msg = mem::take(&mut ui_state.msg);
+                ui_state.log.push(format!("< {msg}"));
+                bufs.push_send(msg.into());
+                ui.memory_mut(|m| m.request_focus(msg_resp.id));
+            }
+
             if ui.button("Disconnect").clicked() {
                 commands.disconnect_sessions("disconnected by user", session);
             }
@@ -71,13 +78,6 @@ fn ui(
                     ui.label(msg);
                 }
             });
-
-            if send_msg {
-                let msg = mem::take(&mut ui_state.msg);
-                ui_state.log.push(format!("< {msg}"));
-                bufs.push_send(msg.into());
-                ui.memory_mut(|m| m.request_focus(msg_resp.id));
-            }
         });
     }
 }
