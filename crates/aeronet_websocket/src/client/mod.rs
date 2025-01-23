9bd5db538cfb768b6cbb2b561dcdfbb011337a9c
aecsocket <aecsocket@tutanota.com>
2025-01-23 19:18:54 +0000

    Fix Clippy 1.84 lints

--- crates/aeronet_websocket/src/client/mod.rs
+++ crates/aeronet_websocket/src/client/mod.rs
@@ -237,7 +237,7 @@ fn should_disconnect(
         Ok(Some(dc_reason)) => Some(dc_reason),
         Err(_) => Some(ClientError::Session(SessionError::BackendClosed).into()),
     };
-    dc_reason.map_or(false, |reason| {
+    dc_reason.is_none_or(|reason| {
         let reason = reason.map_err(anyhow::Error::new);
         commands.trigger_targets(Disconnected { reason }, session);
         true
