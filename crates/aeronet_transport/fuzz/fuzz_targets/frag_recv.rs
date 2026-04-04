22a4ec65f4575838f18e7ccf4ad487adafd593ed
aecsocket <aecsocket@tutanota.com>
2026-04-04 19:50:31 +0100

    fix fuzz

--- crates/aeronet_transport/fuzz/fuzz_targets/frag_recv.rs
+++ crates/aeronet_transport/fuzz/fuzz_targets/frag_recv.rs
@@ -4,17 +4,29 @@ use {
     aeronet_transport::{
         frag::FragmentReceiver,
         packet::{FragmentPosition, MessageSeq},
+        size::MinSize,
     },
     libfuzzer_sys::fuzz_target,
 };
 
-fuzz_target!(|input: (FragmentPosition, &[u8])| {
-    let (position, payload) = input;
+fuzz_target!(|input: (FragmentPosition, MessageSeq, &[u8])| {
+    let (position, msg_seq, payload) = input;
 
-    const MAX_FRAG_LEN: usize = 32;
-    const MEM_LEFT: usize = usize::MAX;
-    const MSG_SEQ: MessageSeq = MessageSeq::new(0);
+    const MAX_FRAG_LEN: MinSize = MinSize(32);
+    // test with 8x the default memory limit
+    // if we set it to `usize`, we can trivially crash the fuzzer
+    // by sending a fragment with a large reported number of fragments
+    const MEM_LEFT: usize = 8 * 4 * 1024 * 1024;
 
     let mut recv = FragmentReceiver::default();
-    _ = recv.reassemble(MAX_FRAG_LEN, MEM_LEFT, MSG_SEQ, position, payload);
+    _ = recv.reassemble(
+        MAX_FRAG_LEN,
+        MEM_LEFT,
+        MessageSeq::new(0),
+        position,
+        payload,
+    );
+
+    let mut recv = FragmentReceiver::default();
+    _ = recv.reassemble(MAX_FRAG_LEN, MEM_LEFT, msg_seq, position, payload);
 });
